aff9a6cc0fa510fddb6721eaaa642a7d1dbb5974
Umoren <samuelumoren365@gmail.com>
2020-04-09 14:34:03 +0100

    Fixed Netlify build

---
+++ src/layout/_partials/footer.vue
@@ -0,0 +1,16 @@
+<template name="component-name">
+    <div>
+        <footer class="text-center text-light">
+
+           <p>Built by <a href="github.com/Umoren" class="text-info" target="_blank"> Umoren Samuel </a> </p>
+        </footer>        
+    </div>
+</template>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@500&display=swap');
+
+    footer{
+        font-family: 'Roboto Mono', monospace;
+    }
+</style>
